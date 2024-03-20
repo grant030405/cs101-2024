e6059f564970bf0e44b39500d5b13a9c16b9f153
grant030405 <146087042+grant030405@users.noreply.github.com>
2024-03-20 23:19:01 +0800

    Update main1.c

--- hw2/main1.c
+++ hw2/main1.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
 int main() {
-    FILE* fp;
-    fp = fopen("lotto.txt", "w+");
-
-    int i=0,n,j ;
+    int i=0,n;
     int num[7];
     srand((unsigned) time(NULL));
     time_t curtime;
@@ -20,26 +18,26 @@ int main() {
     scanf("%d", &n);
     printf("\t以為您購買的 %d 樂透組合輸出至 lotto.txt\n", n);
     
-    
-
+    FILE* fp;
+    fp = fopen("lotto.txt", "w+");
     fprintf(fp,"========  lotto649  ========\n");
     fprintf(fp,"= %s =\n",s);
-    for(j = 1;j < 6;j++) {
+    for(int j = 1;j < 6;j++) {
         fprintf(fp,"[%d]: ",j);
         if(i != n) {
             for (int q = 0;q < 7;q++) {
-        num[q] = rand()%70;
-        fprintf(fp," %d",num[q]);
-        }
-        fprintf(fp,"\n");
-            i++;
+                num[q] = rand()%70;
+                fprintf(fp," %d",num[q]);
+            }
+            fprintf(fp,"\n");
+             i++;
         }
         else {
             fprintf(fp," -- -- -- -- -- -- --\n");
         }
     }
     fprintf(fp,"========  csie@CGU  ========");
-
-    
     fclose(fp);
+    return 0;
 }
+
